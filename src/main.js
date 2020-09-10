6efbef9e3ec90660d1e5ae7704bb51f24613afb3
emir <contact@ekherchi.fr>
2020-09-10 11:45:22 +0200

    add vue fire and script

--- src/main.js
+++ src/main.js
@@ -2,12 +2,26 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
+import router from './router'
+
+import { firestorePlugin } from 'vuefire'
+import firebase from 'firebase/app'
+import 'firebase/firestore'
+Vue.use(firestorePlugin)
+
+firebase.initializeApp({
+  projectId: 'fire-app-b9a22',
+  databaseURL: 'https://fire-app-b9a22.firebaseio.com'
+})
+
+export const db = firebase.firestore()
 
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
+  router,
   components: { App },
   template: '<App/>'
 })
